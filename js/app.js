a69e2e2e87b9ff793346c0d7b8d9bb84b5d8911c
Oscar Omar Gonzalez Toledo <oscar.gonzalez@tautenet.com.mx>
2023-04-11 18:09:54 -0600

    dom-app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+import data from "./data.js"
+import dom from "./dom.js"
 
+
+// ejecuto la funcion getData para traer os datos del back y los guardo en la variable datos
+const datos = await data.getData('./stays.json')
+
+// seleccionamos el contenedor de las cards
+const products = dom.$(".container")
+
+//iteramos los objetos del array
+
+datos.forEach( element => {
+    // Creamos el card con la informacion del elemento
+    const card = dom.newCard(element);
+    console.log(element)
+  
+    // Agregamos el card al elemento products
+    products.appendChild(card);
+  })
