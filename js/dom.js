e2e34b1af7c73b30deeb00387db6810353610126
Oscar Omar Gonzalez Toledo <oscar.gonzalez@tautenet.com.mx>
2023-04-17 08:06:40 -0600

    terminado2

--- js/dom.js
+++ js/dom.js
@@ -10,23 +10,25 @@ const estancias = $(".estancias")
 const newCard = (obj) => {
 
     const div = document.createElement('div');
-
     div.className = 'card-img';
     div.innerHTML = `
-    <div class="img card-img-modified">
+    <div class="img card-img-modified mt-3">
       <img class="w-100 h-100 rounded-4" src="${obj.photo}" alt="${obj.title}">
     </div>
-    <div class="info d-flex justify-content-between mt-3">
-      <p class="d-flex">
-        <span class="fw-bold">${obj.superHost?"Super Host ":""}</span>
-        <span class="fw-bold">${obj.type+" "}${obj.beds !== null?obj.beds+"beds":" "}</span>
-        <span class="material-symbols-outlined">star</span>
+    <div class="info d-flex justify-content-between">
+        <div class="superhost justify-content-between">${obj.superHost?"Super Host ":""}</div>
+        <span>${obj.type}</span>
+        <span>${obj.beds !== null?obj.beds+" beds":" "}</span>
+        <div clas="stars">
+        <span class="start material-symbols-outlined">star</span>
         <span>${obj.rating}</span>
-      </p>
+        </div>
     </div>
-    <span>${obj.title}</span>
+    <div class="title-d fw-bold text-start">${obj.title}</div>
     `
     return div
+
+    
   }
 
   const showCards = (arr) => {
