047bf20f3052d55aa9040871dc3118de8731a40c
Oscar Omar Gonzalez Toledo <oscar.gonzalez@tautenet.com.mx>
2023-04-16 21:04:26 -0600

    terminado

--- js/app.js
+++ js/app.js
@@ -7,24 +7,30 @@ const datos = await data.getData("./stays.json");
 const searchProduct = dom.$("#searchProduct");
 const botonBuscar = dom.$(".boton-buscar");
 const guest = dom.$("#guest");
+const guests = dom.$("#guests")
+const place1 = dom.$(".place");
+const place2 = dom.$("#place");
+const stays = dom.$("#stays");
 
 let filtro = ""
 let numGuest = 0
 
 searchProduct.addEventListener("keyup", () => {
   filtro = searchProduct.value;
-  return filtro
+  place1.innerHTML = filtro
+  place2.innerHTML = filtro
 });
 
 guest.addEventListener("click", () => {
   numGuest = guest.value;
+  guests.innerHTML = numGuest + " guest"
   return numGuest
 });
 
 botonBuscar.addEventListener("click", () => {
   const filtered = filtro === "" ? datos : data.filterByName(datos, filtro);
   const filtered_final = data.filtrarGuest(filtered,numGuest);
-
+  stays.innerHTML=filtered_final.length
   dom.showCards(filtered_final);
 });
 
